fb0217b8fe025b62a356ed049ea6a8605fe9e0ca
Kshamenk Benitez <122121634+Kshamenk@users.noreply.github.com>
2024-03-01 18:09:08 -0300

    Ready for its presentation

--- pfg_project/src/components/ProductList.vue
+++ pfg_project/src/components/ProductList.vue
@@ -1,6 +1,7 @@
-<!-- 
+
+
 <template>
-  <div>
+  <div class="container">
     <h1 class="product-list-title">Explore Our Products :</h1>
     <div class="product-list">
       <div v-for="product in products" :key="product.id" class="product-card">
@@ -12,96 +13,9 @@
         </div>
       </div>
     </div>
-  </div>
-</template>
-  
-<script>
-export default {
-  data() {
-    return {
-      products: [
-        { id: 1, name: 'Samgung Galaxy S20', price: 20.99, image: '/images/smartphone.jpg' },
-        { id: 2, name: 'Play-Station 5', price: 300.49, image: '/images/playok.jpg' },
-
-      ]
-    };
-  }
-}
-</script>
-  
-<style scoped>
-.product-list-title {
-  font-size: 32px;
-  color: #3498db;
-  margin-bottom: 30px;
-  text-align: center;
-}
-
-.product-list {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-}
-
-.product-card {
-  border: 1px solid #ddd;
-  border-radius: 8px;
-  padding: 15px;
-  margin: 10px;
-  width: 250px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  transition: transform 0.2s ease-in-out;
-  background-color: #f0f0f0;
-}
-
-.product-card:hover {
-  transform: scale(1.05);
-}
-
-.product-image {
-  max-width: 100%;
-  max-height: 250px;
-  margin-bottom: 10px;
-}
-
-.product-details {
-  text-align: left;
-
-}
-
-.product-price {
-  font-weight: normal;
-}
-
-.view-details-link {
-  color: #fff;
-  background-color: #3498db;
-  padding: 10px;
-  border: none;
-  border-radius: 5px;
-  text-decoration: none;
-  display: inline-block;
-  transition: background-color 0.3s ease;
-}
-
-.view-details-link:hover {
-  background-color: #2980b9;
-}
-</style> -->
-
-<template>
-  <div>
-    <h1 class="product-list-title">Explore Our Products :</h1>
-    <div class="product-list">
-      <div v-for="product in products" :key="product.id" class="product-card">
-        <img :src="product.image" alt="Product Image" class="product-image">
-        <div class="product-details">
-          <h3>{{ product.name }}</h3>
-          <p class="product-price">Price: ${{ product.price }}</p>
-          <router-link :to="'/products/' + product.id" class="view-details-link">View Details</router-link>
-        </div>
-      </div>
-    </div>
+    <button type="submit" class="view-details-link" @click="goToRepository" :disabled="isButtonDisabled">
+      Go to Repository
+    </button>
   </div>
 </template>
   
@@ -111,7 +25,8 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      products: []
+      products: [],
+      isButtonDisabled: false
     };
   },
   mounted() {
@@ -123,22 +38,34 @@ export default {
       .catch(error => {
         console.error('Error fetching products:', error);
       });
+  },
+  methods: {
+    goToRepository() {
+      this.isButtonDisabled = true;
+      window.location.href = 'https://github.com/Kshamenk/PFG-Test1';
+      setTimeout(() => {
+        this.isButtonDisabled = false;
+      }, 1000);
+    }
   }
 }
 </script>
   
 <style scoped>
+
 .product-list-title {
   font-size: 32px;
   color: #3498db;
   margin-bottom: 30px;
   text-align: center;
+ 
 }
 
 .product-list {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-evenly;
+ 
 }
 
 .product-card {
@@ -149,7 +76,7 @@ export default {
   width: 250px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   transition: transform 0.2s ease-in-out;
-  background-color: #f0f0f0;
+ background-color: rgb(240, 239, 231);
 }
 
 .product-card:hover {
@@ -179,6 +106,8 @@ export default {
   text-decoration: none;
   display: inline-block;
   transition: background-color 0.3s ease;
+  margin-top: 16px;
+  cursor: pointer;
 }
 
 .view-details-link:hover {
